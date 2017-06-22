33629532f66cb8a87c4b139e1b956e5a6bbd3d07
SkalskiP <pskalski@student.agh.edu.pl>
2017-06-22 23:47:30 +0200

    full task from first lecture

--- 01_Introduction_and_Optimization_Problems/Task_1_Food_Menu.py
+++ 01_Introduction_and_Optimization_Problems/Task_1_Food_Menu.py
@@ -1,3 +1,5 @@
+#CLASS REPRESENTATION OF OBJECTS IN MENU
+
 class Food(object):
     def __init__(self, n, v , w):
         self.name = n
@@ -15,3 +17,61 @@ class Food(object):
 
     def __str__(self):
         return self.name + ': <' + str(self.value) + ', ' + str(self.calories) + '>'
+
+# FUNCTION RESPONSIBLE FOR BUILDING MENU
+
+def buildMenu(names, values, calories):
+    """names, values, calories lists of same length.
+    name a list of strings
+    values and calories lists of numbers
+    returns list of Foods"""
+    menu = []
+    for i in range(len(values)):
+        menu.append(Food(names[i], values[i], calories[i]))
+
+    return menu
+
+# IMPLEMENTATION OF FLEXIBLE GREEDY
+
+def greedy(items, maxCost, keyFunction):
+    """Assumes items a list, maxCost >= 0,
+    keyFunstion maps elements of items to numbers"""
+    itemsCopy = sorted(items, key = keyFunction, reverse = True)
+    # sorting from best to worst by value of keyFunction
+
+    result = []
+    totalValue, totalCost = 0.0, 0.0
+
+    for i in range(len(itemsCopy)):
+        if (totalCost + itemsCopy[i].getCost()) <= maxCost:
+            result.append(itemsCopy[i])
+            totalCost += itemsCopy[i].getCost()
+            totalValue += itemsCopy[i].getValue()
+
+    return (result, totalValue)
+
+# USING GREEDY
+
+def testGreedy(items, constraint, keyFunction):
+    taken, val = greedy(items, constraint, keyFunction)
+    print('Total value of items taken = ', val)
+    for item in taken:
+        print(' ', item)
+
+def testGreedys(foods, maxUnits):
+    print('Use greedy by value to allocate', maxUnits, 'calories')
+    testGreedy(foods, maxUnits, Food.getValue)
+    # it is passing function; that's why there is no () at the end
+
+    print('\nUse greedy by cost to allocate', maxUnits, 'calories')
+    testGreedy(foods, maxUnits, lambda x: 1/Food.getCost(x))
+    #lambda is used to make anonymous functions, with no name
+
+    print('\nUse greedy by density to allocate', maxUnits, 'calories')
+    testGreedy(foods, maxUnits, Food.density)
+
+names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'cola', 'apple', 'donut', 'cake']
+values = [89, 90, 95, 100, 90, 79, 50, 10]
+calories = [123, 154, 258, 354, 365, 150, 95, 195]
+foods = buildMenu(names, values, calories)
+testGreedys(foods, 750)
