5ee31c8a960da085457f2e69613801b714d39cd9
SkalskiP <pskalski@student.agh.edu.pl>
2017-06-27 17:55:37 +0200

    Dynamic programing solution to 0/1 knapsack problem

---
+++ 02_Optimization_Problems/lecture2.py
@@ -0,0 +1,184 @@
+
+
+class Food(object):
+    def __init__(self, n, v, w):
+        self.name = n
+        self.value = v
+        self.calories = w
+    def getValue(self):
+        return self.value
+    def getCost(self):
+        return self.calories
+    def density(self):
+        return self.getValue()/self.getCost()
+    def __str__(self):
+        return self.name + ': <' + str(self.value)\
+                 + ', ' + str(self.calories) + '>'
+
+def buildMenu(names, values, calories):
+    menu = []
+    for i in range(len(values)):
+        menu.append(Food(names[i], values[i],
+                          calories[i]))
+    return menu
+
+def greedy(items, maxCost, keyFunction):
+    """Assumes items a list, maxCost >= 0,
+         keyFunction maps elements of Items to numbers"""
+    itemsCopy = sorted(items, key = keyFunction,
+                       reverse = True)
+    result = []
+    totalValue, totalCost = 0.0, 0.0
+    for i in range(len(itemsCopy)):
+        if (totalCost+itemsCopy[i].getCost()) <= maxCost:
+            result.append(itemsCopy[i])
+            totalCost += itemsCopy[i].getCost()
+            totalValue += itemsCopy[i].getValue()
+    return (result, totalValue)
+
+def testGreedy(items, constraint, keyFunction):
+    taken, val = greedy(items, constraint, keyFunction)
+    print('Total value of items taken =', val)
+    for item in taken:
+        print('   ', item)
+
+def testGreedys(foods, maxUnits):
+    print('Use greedy by value to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits, Food.getValue)
+    print('\nUse greedy by cost to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits,
+               lambda x: 1/Food.getCost(x))
+    print('\nUse greedy by density to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits, Food.density)
+
+def maxVal(toConsider, avail):
+    """Assumes toConsider a list of items, avail a weight
+       Returns a tuple of the total value of a solution to the
+         0/1 knapsack problem and the items of that solution"""
+    if toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getCost() > avail:
+        #Explore right branch only
+        result = maxVal(toConsider[1:], avail)
+    else:
+        nextItem = toConsider[0]
+        #Explore left branch
+        withVal, withToTake = maxVal(toConsider[1:],
+                                     avail - nextItem.getCost())
+        withVal += nextItem.getValue()
+        #Explore right branch
+        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+        #Choose better branch
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    return result
+
+def testMaxVal(foods, maxUnits, printItems = True):
+    print('Use search tree to allocate', maxUnits,
+          'calories')
+    val, taken = maxVal(foods, maxUnits)
+    print('Total value of items taken =', val)
+    if printItems:
+        for item in taken:
+            print('   ', item)
+
+names = ['wine', 'beer', 'pizza', 'burger', 'fries',
+         'cola', 'apple', 'donut', 'cake']
+values = [89,90,95,100,90,79,50,10]
+calories = [123,154,258,354,365,150,95,195]
+foods = buildMenu(names, values, calories)
+
+testGreedys(foods, 750)
+print('')
+testMaxVal(foods, 750)
+
+import random
+
+def buildLargeMenu(numItems, maxVal, maxCost):
+    items = []
+    for i in range(numItems):
+        items.append(Food(str(i),
+                          random.randint(1, maxVal),
+                          random.randint(1, maxCost)))
+    return items
+
+#for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45):
+#    print('Try a menu with', numItems, 'items')
+#    items = buildLargeMenu(numItems, 90, 250)
+#    testMaxVal(items, 750, False)  
+
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+#for i in range(121):
+#    print('fib(' + str(i) + ') =', fib(i))
+
+
+def fastFib(n, memo = {}):
+    """Assumes n is an int >= 0, memo used only by recursive calls
+       Returns Fibonacci of n"""
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except KeyError:
+        result = fastFib(n-1, memo) + fastFib(n-2, memo)
+        memo[n] = result
+        return result
+
+#for i in range(121):
+#    print('fib(' + str(i) + ') =', fastFib(i))
+
+def fastMaxVal(toConsider, avail, memo = {}):
+    """Assumes toConsider a list of subjects, avail a weight
+         memo supplied by recursive calls
+       Returns a tuple of the total value of a solution to the
+         0/1 knapsack problem and the subjects of that solution"""
+    if (len(toConsider), avail) in memo:
+        result = memo[(len(toConsider), avail)]
+        #Check if similar problem was already solved. If it was take the solution.
+    elif toConsider == [] or avail == 0:
+        result = (0, ())
+        #If list of elements to consider is empty or we don't have any more space we can not add any more elements.
+    elif toConsider[0].getCost() > avail:
+        #If the cost of element is to big we wxplore right branch only
+        result = fastMaxVal(toConsider[1:], avail, memo)
+    else:
+        nextItem = toConsider[0]
+        #Explore left branch
+        withVal, withToTake =\
+                 fastMaxVal(toConsider[1:],
+                            avail - nextItem.getCost(), memo)
+        withVal += nextItem.getValue()
+        #Explore right branch
+        withoutVal, withoutToTake = fastMaxVal(toConsider[1:],
+                                                avail, memo)
+        #Choose better branch
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    memo[(len(toConsider), avail)] = result
+    return result
+
+def testMaxVal(foods, maxUnits, algorithm, printItems = True):
+    print('Menu contains', len(foods), 'items')
+    print('Use search tree to allocate', maxUnits,
+          'calories')
+    val, taken = algorithm(foods, maxUnits)
+    if printItems:
+        print('Total value of items taken =', val)
+        for item in taken:
+            print('   ', item)
+          
+for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45, 50):
+   items = buildLargeMenu(numItems, 90, 250)
+   testMaxVal(items, 750, fastMaxVal, True)
